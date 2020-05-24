68bbc80c6580e07a67259f70fae42627d3899760
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-05-24 15:56:33 -0400

    Going to restructure the models section.

--- backend/app/main/model/Category.py
+++ backend/app/main/model/Category.py
@@ -1,7 +1,9 @@
 from .. import db, flask_bcrypt
 
-class Category(db.Model):
-  __tablename__ = 'categories'
 
-  id = db.Column(db.Integer, primary_key=True)
-  type = db.Column(db.String)
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String, nullable=False)
+    questions = db.relationship('Question', backref='category', lazy=True)
--- backend/app/main/model/Question.py
+++ backend/app/main/model/Question.py
@@ -4,7 +4,7 @@ class Question(db.Model):
     __tablename__ = 'questions'
 
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String)
-    answer = db.Column(db.String)
-    category = db.Column(db.String)
-    difficulty = db.Column(db.Integer)
+    question = db.Column(db.String, nullable=False)
+    answer = db.Column(db.String, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('Category.id'), nullable=False)
+    difficulty = db.Column(db.Integer, nullable=False)
